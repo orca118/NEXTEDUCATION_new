b28bc9d5c5b6a18c0f9b2493947bf2a75576aa5f
orca118 <61328198+orca118@users.noreply.github.com>
2026-09-01 09:46:03 -0400

    Add basename to BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -11,13 +12,16 @@ import Contact from './pages/Contact';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/NEXTEDUCATION_new">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="programs" element={<Programs />} />
-          <Route path="student-mentor-program" element={<StudentMentorProgram />} />
+          <Route
+            path="student-mentor-program"
+            element={<StudentMentorProgram />}
+          />
           <Route path="orca-robotics" element={<OrcaRobotics />} />
           <Route path="get-involved" element={<GetInvolved />} />
           <Route path="donate" element={<Donate />} />
